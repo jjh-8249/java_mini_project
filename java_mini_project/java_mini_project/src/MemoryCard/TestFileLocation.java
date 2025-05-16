619ed32ffa604bfbc506144298178594ac2ed592
jjh-8249 <wkdwnsgur8249@naver.com>
2025-05-16 16:45:32 +0900

    Delete java_mini_project/java_mini_project/src/MemoryCard directory

--- java_mini_project/java_mini_project/src/MemoryCard/TestFileLocation.java
+++
@@ -1,9 +0,0 @@
-package MemoryCard;
-
-public class TestFileLocation {
-    public static void main(String[] args) {
-        // 현재 실행 디렉토리 확인
-        String currentDir = System.getProperty("user.dir");
-        System.out.println("현재 실행 디렉토리: " + currentDir);
-    }
-}
